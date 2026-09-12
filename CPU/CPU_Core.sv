5d99c025bd4ff0b2feef41d0e8c57a8d3a3bb766
saint-cuauhtlatoatzin <05juandiegorodriguez@gmail.com>
2026-09-12 16:48:38 -0700

    Update CPU_Core.sv

--- CPU/CPU_Core.sv
+++ CPU/CPU_Core.sv
@@ -189,24 +189,3 @@ registerfile RF0(
     );
 
 endmodule
-
-module pc_counter (
-    input wire clk,
-    input wire EN,
-    input wire RST,
-    input wire LOAD,
-    input wire [7:0] LOAD_ADDRESS,
-    output logic [7:0] PC
-);
-
-  always_ff @(posedge clk or posedge RST) begin 
-if (RST) PC <= 8'b00000000; 
-
-else if (LOAD) PC <= LOAD_ADDRESS; 
-
-else if (EN) PC <= PC + 1; 
-
-else PC <= PC; 
-
-end 
-endmodule 

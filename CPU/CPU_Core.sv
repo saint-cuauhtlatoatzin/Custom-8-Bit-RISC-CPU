0b372b84d44bcc9f01ba5d7975a3b97e8640b97d
saint-cuauhtlatoatzin <05juandiegorodriguez@gmail.com>
2026-09-12 16:47:39 -0700

    Update CPU_Core.sv

--- CPU/CPU_Core.sv
+++ CPU/CPU_Core.sv
@@ -1,3 +1,5 @@
+// Code your design here
+// Code your design here
 `timescale 1ns/1ps
 
 module cpu_core(
@@ -34,3 +36,177 @@ module cpu_core(
     logic Carry;
     logic Overflow;
     logic Zero;
+
+    //--------------------------------------------------
+    // Control Signals
+    //--------------------------------------------------
+
+    logic PC_ENABLE;
+    logic IR_LOAD;
+    logic REG_WRITE;
+    logic PC_LOAD;
+    logic HALT;
+    logic ALU_ENABLE;
+  
+  	logic [3:0] ALU_CONTROL;
+
+    //--------------------------------------------------
+    // Zero Flag
+    //--------------------------------------------------
+
+    assign Zero = (Result == 8'b00000000);
+
+    //--------------------------------------------------
+    // Program Counter
+    //--------------------------------------------------
+
+    pc_counter PC0(
+
+        .clk(clk),
+        .EN(PC_ENABLE),
+        .RST(RST),
+
+        .LOAD(PC_LOAD),
+        .LOAD_ADDRESS(8'b00000000),
+
+        .PC(PC)
+
+    );
+
+    //--------------------------------------------------
+    // Instruction ROM
+    //--------------------------------------------------
+
+    instruction_rom ROM0(
+
+        .ADDRESS(PC),
+
+        .INSTRUCTION(instruction)
+
+    );
+
+    //--------------------------------------------------
+    // Instruction Register
+    //--------------------------------------------------
+
+    instruction_register IR0(
+
+        .clk(clk),
+        .RST(RST),
+
+        .LOAD(IR_LOAD),
+
+        .INSTRUCTION_IN(instruction),
+
+        .INSTRUCTION_OUT(instruction_reg)
+
+    );
+
+    //--------------------------------------------------
+    // Decoder
+    //--------------------------------------------------
+
+    decoder DEC0(
+
+        .INSTRUCTION(instruction_reg),
+
+        .OPCODE(opcode),
+
+        .RD(rd),
+        .RS1(rs1),
+        .RS2(rs2)
+
+    );
+
+    //--------------------------------------------------
+    // Register File
+    //--------------------------------------------------
+
+registerfile RF0(
+    .clk(clk),
+    .EN(REG_WRITE),
+    .RST(RST),
+
+    .WRITE_SEL(rd),
+
+    .READ_SEL_A(rs1),
+    .READ_SEL_B(rs2),
+
+    .D(Result),
+
+    .DEBUG_LOAD(DEBUG_LOAD),
+    .DEBUG_REG_SEL(DEBUG_REG_SEL),
+    .DEBUG_VALUE(DEBUG_VALUE),
+
+    .QA(QA),
+    .QB(QB)
+);
+    // ALU
+    //--------------------------------------------------
+
+    ALU ALU0(
+
+        .A(QA),
+        .B(QB),
+
+        .ALU_CONTROL(ALU_CONTROL),
+
+        .Result(Result),
+
+        .CARRY(Carry),
+
+        .OVERFLOW(Overflow)
+
+    );
+
+    //--------------------------------------------------
+    // Control FSM
+    //--------------------------------------------------
+
+    finite_state_machine CTRL(
+
+        .clk(clk),
+        .rst(RST),
+
+        .opcode(opcode),
+
+        .carry(Carry),
+        .overflow(Overflow),
+        .zero(Zero),
+
+        .PC_ENABLE(PC_ENABLE),
+        .IR_LOAD(IR_LOAD),
+        .REG_WRITE(REG_WRITE),
+
+        .ALU_CONTROL(ALU_CONTROL),
+
+        .PC_LOAD(PC_LOAD),
+
+        .HALT(HALT),
+
+        .ALU_ENABLE(ALU_ENABLE)
+
+    );
+
+endmodule
+
+module pc_counter (
+    input wire clk,
+    input wire EN,
+    input wire RST,
+    input wire LOAD,
+    input wire [7:0] LOAD_ADDRESS,
+    output logic [7:0] PC
+);
+
+  always_ff @(posedge clk or posedge RST) begin 
+if (RST) PC <= 8'b00000000; 
+
+else if (LOAD) PC <= LOAD_ADDRESS; 
+
+else if (EN) PC <= PC + 1; 
+
+else PC <= PC; 
+
+end 
+endmodule 
